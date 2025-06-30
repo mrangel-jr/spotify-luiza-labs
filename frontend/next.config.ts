ca0f6670cc7c201b4b50d3fc0195858b6e78f180
Marcos Rangel <mrangel@gmail.com>
2025-06-30 19:19:37 -0300

    fix: Next config

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "standalone", // Importante para deploy
+  experimental: {
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+  env: {
+    CUSTOM_KEY: process.env.CUSTOM_KEY,
+  },
 };
 
 export default nextConfig;
